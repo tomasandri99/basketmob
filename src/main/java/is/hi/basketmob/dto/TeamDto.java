b3112e74faaf0409e60b122786235ffd8e6f52ff
tomasandri99 <143426841+tomasandri99@users.noreply.github.com>
2025-10-23 10:03:07 +0000

    Revert "feat: auth + Favorites MVP"

--- src/main/java/is/hi/basketmob/dto/TeamDto.java
+++ src/main/java/is/hi/basketmob/dto/TeamDto.java
@@ -1,18 +1,4 @@
 package is.hi.basketmob.dto;
 
-/** Simplified Team data for use in responses (e.g., favorites list, game details). */
-public class TeamDto {
-    public Long id;
-    public String name;
 
-    public TeamDto() {}
-    public TeamDto(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-    // Convenience constructor, in case we only have name
-    public TeamDto(String name) {
-        this.id = null;
-        this.name = name;
-    }
-}
+public record TeamDto(Long id, String name) {}
